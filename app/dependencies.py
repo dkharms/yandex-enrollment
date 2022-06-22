1cdc4b4f4092dd3da3b839997cd26cfc37e8f171
dkharms <dkharms@dkharms-macbook.local>
2022-06-22 19:57:49 +0300

    feat(dependecies): added simple logger config

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,9 +1,13 @@
+import logging
+import typing as t
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
+                       "check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
@@ -13,3 +17,25 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+class Logger(object):
+
+    def __init__(self) -> None:
+        formatter = logging.Formatter(
+            '[%(levelname)s]:%(asctime)s:%(filename)s:%(funcName)s: %(message)s')
+
+        self.log = logging.getLogger("app")
+        self.log.setLevel(logging.DEBUG)
+
+        ch = logging.FileHandler("info.log")
+        ch.setLevel(logging.DEBUG)
+        ch.setFormatter(formatter)
+
+        self.log.addHandler(ch)
+
+    def __call__(self) -> t.Any:
+        yield self.log
+
+
+logger = Logger()
