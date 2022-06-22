a1b37820b8455c002719adab3b1bf393fbdd6cee
dkharms <dkharms@dkharms-macbook.local>
2022-06-22 19:59:06 +0300

    feat(app): added startup configuration

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ from app.routers import imports_router
 from app.models import Base
 from app.dependencies import engine
 
-Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Yandex Enrollment", version="1.0.0")
 app.include_router(imports_router)
+
+
+@app.on_event("startup")
+def create_tables():
+    Base.metadata.create_all(bind=engine)
