0ed35bb52eddaaeb55a3c19231576bdf0aa1f0b6
dkharms <dkharms@dkharms-macbook.local>
2022-06-22 13:34:43 +0300

    feat(schemas): added validators for schemas

--- app/schemas/shop_unit_import.py
+++ app/schemas/shop_unit_import.py
@@ -6,19 +6,38 @@ from datetime import datetime
 
 from .shop_unit import ShopUnitType
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class ShopUnitImport(BaseModel):
     id: UUID = Field(description="Unique identifier.")
     name: str = Field(description="Category or product name.")
-    parentId: t.Union[None, UUID] = Field(description="Id of parent category.")
+    parent_id: t.Union[None, UUID] = Field(
+        description="Id of parent category.", alias="parentId")
     type: ShopUnitType = Field(description="Product or category type.")
     price: t.Union[None, int] = Field(
         description="Average price for category or price of product.")
+
+    @validator("price")
+    def validate_price(cls, price, values):
+        if values["type"] == ShopUnitType.category and price != None:
+            raise ValueError("Category must have null price!")
+        if price is None or price < 0:
+            raise ValueError("Product price must be >= 0!")
+        return price
 
 
 class ShopUnitImportRequest(BaseModel):
     items: t.List[ShopUnitImport] = Field(
         description="List of products and categories.")
-    updateDate: datetime = Field(description="Update datetime.")
+    update_date: datetime = Field(
+        description="Update datetime.", alias="updateDate")
+
+    @validator("update_date")
+    def validate_update_date(cls, update_date):
+        try:
+            datetime.strptime(update_date, "%Y-%m-%dT%H:%M:%S.%fZ")
+        except Exception as e:
+            raise ValueError("Wrong update date format!")
+        finally:
+            return update_date
