3c114cfcef865b22629671530fa752beb1b81d13
dkharms <dkharms@dkharms-macbook.local>
2022-06-23 11:44:10 +0300

    fix(imports): now updating parent entities if child was updated

--- app/routers/imports.py
+++ app/routers/imports.py
@@ -1,16 +1,16 @@
-import logging
 import typing as t
 
+from logging import Logger
 from datetime import datetime
 
 from fastapi import APIRouter, Depends, status, Response
-from fastapi.exceptions import HTTPException
+from fastapi.exceptions import RequestValidationError
 from sqlalchemy.orm import Session
 
 import app.schemas as s
 import app.models as m
 
-from app.utils import LoggerProxy, DatabaseProxy, ConfigProxy
+from app.utils import LoggerProxy, DatabaseProxy
 
 imports_router = APIRouter(tags=["Must Have Endpoints"])
 
@@ -61,47 +61,61 @@ def validate_unique_ids(items: t.List[s.ShopUnitImport]):
 
     for item in items:
         if item.id in seen_ids:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="Ids must be unique!")
+            raise RequestValidationError(errors=["Validation Failed"])
         seen_ids.add(item.id)
 
     return seen_ids
 
 
-def validate_parent_entity(items: t.List[s.ShopUnitImport], shop_unit_graph: ShopUnitGraph, db: Session):
+def validate_parent_entity(
+        items: t.List[s.ShopUnitImport], shop_unit_graph: ShopUnitGraph,
+        db: Session, log: Logger
+):
     for item in items:
+        log.info(
+            f"validating parent entity: id={item.id} parent_id={item.parent_id}"
+        )
+
         if item.parent_id is None:
             continue
 
         if item.parent_id in shop_unit_graph:
             parent_item = shop_unit_graph[item.parent_id]
             if parent_item.type == s.ShopUnitType.offer:
-                raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST, detail="Parent entity must category!")
+                raise RequestValidationError(errors=["Validation Failed"])
             continue
 
         item_model = db.get(m.ShopUnit, str(item.parent_id))
         if item_model is None:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="Parent entity must exist!")
+            raise RequestValidationError(errors=["Validation Failed"])
 
         if item_model.type == s.ShopUnitType.offer:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="Parent entity must be category!")
+            raise RequestValidationError(errors=["Validation Failed"])
 
 
-def validate_type_consistency(items: t.List[s.ShopUnitImport], db: Session):
+def validate_type_consistency(items: t.List[s.ShopUnitImport], db: Session, log: Logger):
     for item in items:
+        log.info(f"validating type consistency: id={item.id}")
+
         item_model = db.get(m.ShopUnit, str(item.id))
         if item_model is not None and item_model.type != item.type:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="Entity type must be consistent!")
+            raise RequestValidationError(errors=["Validation Failed"])
 
 
-def create_or_update(item: s.ShopUnitImport, update_date: datetime, db: Session, log: logging.Logger):
-    log.info(f"updating or creating {item.id=} {item.name=} {item.type=}")
+def update_parent_date(item: m.ShopUnit, update_date: datetime, db: Session, log: Logger):
+    log.info(f"updating item: id={item.id} update_date={update_date}")
 
+    item.date = update_date  # pyright: ignore
+    db.commit()
+
+    log.info(f"updated item: id={item.id} update_date={update_date}")
+    if item.parent is not None:
+        update_parent_date(item.parent, update_date, db, log)
+
+
+def create_or_update(item: s.ShopUnitImport, update_date: datetime, db: Session, log: Logger):
     item_model = db.get(m.ShopUnit, str(item.id))
+
     if item_model is not None:
         item_model.name = item.name
         item_model.date = update_date
@@ -109,33 +123,30 @@ def create_or_update(item: s.ShopUnitImport, update_date: datetime, db: Session,
         item_model.price = item.price
     else:
         item_model = m.ShopUnit(
-            id=str(item.id),
-            name=item.name,
-            date=update_date,
-            parent_id=str(item.parent_id),
-            type=item.type,
-            price=item.price,
+            id=str(item.id), name=item.name,
+            date=update_date, parent_id=str(item.parent_id),
+            type=item.type, price=item.price,
         )
-
-    db.add(item_model)
+        db.add(item_model)
     db.commit()
 
-    log.info(f"finished updating or creating {item.id=}")
+    update_parent_date(item_model, update_date, db, log)
 
 
 @imports_router.post("/imports")
 async def import_entities(
         import_request: s.ShopUnitImportRequest,
-        db: Session = Depends(DatabaseProxy), log=Depends(LoggerProxy)
+        db: Session = Depends(DatabaseProxy), log: Logger = Depends(LoggerProxy)
 ):
+    log.info(f"got request for import: amount={len(import_request.items)}")
+
     shop_unit_graph = ShopUnitGraph(import_request.items)
     shop_unit_graph.sort()
 
     validate_unique_ids(import_request.items)
-    validate_type_consistency(import_request.items, db)
-    validate_parent_entity(import_request.items, shop_unit_graph, db)
+    validate_type_consistency(import_request.items, db, log)
+    validate_parent_entity(import_request.items, shop_unit_graph, db, log)
 
-    log.info(f"updating or creating {len(import_request.items)} items")
     for item in shop_unit_graph:
         create_or_update(item, import_request.update_date, db, log)
 
