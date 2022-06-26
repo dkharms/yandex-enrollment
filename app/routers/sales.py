b296a99dd123e21fc689a2e889f66a8e9300355f
dkharms <dkharmsd@gmail.com>
2022-06-26 10:55:32 +0300

    fix(sales): fixed schema interaction

--- app/routers/sales.py
+++ app/routers/sales.py
@@ -22,26 +22,25 @@ def validate_date(date: str):
         raise RequestValidationError("Wrong update date format!")
 
 
-@sales_router.get("/sales", response_model=s.ShopUnitSales)
+@sales_router.get("/sales", response_model=s.ShopUnitStatisticResponse)
 async def sales(date: str, db: Session = Depends(DatabaseProxy), log: logging.Logger = Depends(LoggerProxy)):
     log.info(f"got sales request: {date=}")
 
-    date_obj = validate_date(date)
-    from_time = date_obj - timedelta(days=1)
+    to_time = validate_date(date)
+    from_time = to_time - timedelta(days=1)
 
     unit_models = db.query(m.ShopUnit).filter(
         (m.ShopUnit.type == s.ShopUnitType.offer) &
-        (from_time <= m.ShopUnit.date) & (m.ShopUnit.date <= date_obj)
+        (from_time <= m.ShopUnit.date) & (m.ShopUnit.date <= to_time)
     ).all()
 
-    response_schema = s.ShopUnitSales(items=[])
+    response_schema = s.ShopUnitStatisticResponse(items=[])
     for unit_model in unit_models:
-        unit_schema = s.ShopUnit.from_orm(unit_model)
+        unit_schema = s.ShopUnitStatisticUnit.from_orm(unit_model)
 
         if unit_model.parent_id is not None:
             unit_schema.parent_id = unit_model.parent_id
 
-        unit_schema.children = None
         response_schema.items.append(unit_schema)
 
     return response_schema
